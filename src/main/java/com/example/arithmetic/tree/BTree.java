3e03860d250cca95750cd1a629430244af0f4529
caoyuanhua <caoyuanhua@houbank.com>
2018-06-28 14:38:54 +0800

    tree

--- src/main/java/com/example/arithmetic/tree/BTree.java
+++ src/main/java/com/example/arithmetic/tree/BTree.java
@@ -21,60 +21,55 @@ public class BTree {
     //插入Node
     //插入之前需要判断是否为null
     //为null需要比较大小直到currentNode为null就插入
-    public void insert(int iData,double dData )
-    {
-        //创建node节点
-        Node newNode=new Node();
-        newNode.iData=iData;
-        newNode.dData=dData;
-        //判断root node是否为null
-        if(root==null)
-        {
-            root=newNode;
-        }
-        //不为null
-        else
-        {
-            Node current=root;
-            Node parent;
-            while(true)
-            {
-                parent=current;//保存当current变为null之前的那一个父节点
-                if(iData<current.iData)//插入左节点
-                {
-                    current=current.leftNode;//不断向左node寻找是否为null
-                    if(current==null)
-                    {
-                        parent.leftNode=newNode;
+    public void insert(int iData) {
+        Node newNode = new Node(iData);
+        if (root == null) {
+            root = newNode;
+        } else {
+            Node curentNode = root;
+            Node parentNode;
+            while (true) {
+                parentNode = curentNode;
+                if (iData < curentNode.iData) {
+                    curentNode = curentNode.leftNode;
+                    if (curentNode == null) {
+                        parentNode.leftNode = newNode;
                         return;
                     }
-
-                }
-                //插入右节点
-                else
-                {
-                    current=current.rightNode;
-                    if(current==null)
-                    {
-                        parent.rightNode=newNode;
+                } else {
+                    curentNode = curentNode.rightNode;
+                    if (curentNode == null) {
+                        parentNode.rightNode = newNode;
                         return;
                     }
                 }
-
             }
 
         }
     }
+    public Node find(int key){
+        Node curNode=root;
+        while (curNode.iData!=key){
+            if (key<curNode.iData){
+                curNode=curNode.leftNode;
+            }else {
+                curNode=curNode.rightNode;
+            }
+        }
+        return  curNode;
+
+    }
 
     public static void main(String[] args) {
-        BTree b=new BTree();
-        b.insert(3,3.1);
-        b.insert(1,1.1);
-        b.insert(2,1.1);
-        b.insert(4,1.1);
-        b.insert(5,1.1);
-        b.insert(6,1.1);
+        BTree b = new BTree();
+        b.insert(6);
+        b.insert(5);
+        b.insert(4);
+        b.insert(3);
+        b.insert(2);
+        b.insert(1);
         System.out.println(JSON.toJSONString(b.root));
+        System.out.println(b.find(3).iData);
 
     }
 }
--- src/main/java/com/example/arithmetic/tree/Node.java
+++ src/main/java/com/example/arithmetic/tree/Node.java
@@ -1,25 +1,44 @@
 package com.example.arithmetic.tree;
 
 public class Node {
-    public  int iData;
+    public int iData;
 
-    public  double dData;
+    public Node leftNode;
 
-    public  Node leftNode;
+    public Node rightNode;
 
-    public  Node rightNode;
+    public Node(int iData) {
+        this.iData = iData;
+    }
 
-    public void show(){
-        System.out.println(
-                "{"+iData+","+dData+"}"
-        );
+    public int getiData() {
+        return iData;
+    }
+
+    public void setiData(int iData) {
+        this.iData = iData;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public Node getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(Node rightNode) {
+        this.rightNode = rightNode;
     }
 
     @Override
     public String toString() {
         return "Node{" +
                 "iData=" + iData +
-                ", dData=" + dData +
                 ", leftNode=" + leftNode +
                 ", rightNode=" + rightNode +
                 '}';
