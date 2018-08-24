6c145ccee3a8b3bdabf0e3f3672f0d79425525fc
caoyuanhua <caoyuanhua@houbank.com>
2018-08-24 14:58:23 +0800

    mid

--- src/main/java/com/example/arithmetic/sort/BinaryInsertSort.java
+++ src/main/java/com/example/arithmetic/sort/BinaryInsertSort.java
@@ -21,28 +21,53 @@ public class BinaryInsertSort {
         }
     }
 
+    /**
+     * 1 2 3 4 5 6 7 3
+     * i=7;
+     * left=0
+     * right=6;
+     * midv=3;
+     * mid>temp
+     *right=mid-1=2
+     * mid=1
+     * midv=2;
+     * midv<temp
+     * left=1+1=2;
+     * mid=2;
+     * midv=3;
+     * temp=midv=3
+     * right=1;
+     * left>right
+     * left=2
+     *
+     *
+     *
+     * @param a
+     */
     private static void sort(int[] a)
     {
         for (int i=0;i<a.length;i++){
-            int temp=a[i];
             int left=0;
             int right=i-1;
+            int temp=a[i];
             int mid;
             while (left<=right){
-                 mid=(left+right)/2;
-                if (temp>a[mid]){
-                    left=mid+1;
-                }else {
+                mid=(left+right)/2;
+                if (a[mid]>temp){
                     right=mid-1;
+                }else {
+                    left=mid+1;
                 }
             }
             for (int j=i-1;j>=left;j--){
                 a[j+1]=a[j];
             }
             a[left]=temp;
-
-
         }
+
+
+
+
 
 
     }
--- src/main/java/com/example/arithmetic/sort/DirectInsertSort.java
+++ src/main/java/com/example/arithmetic/sort/DirectInsertSort.java
@@ -7,24 +7,30 @@ public class DirectInsertSort {
         int[] a = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 1 };
         System.out.println("排序之前：");
 
-        for (int i = 1; i <a.length ; i++) {
-            int temp=a[i];
-            int j;
-            for ( j=i-1;j>=0;j--){
-                if (a[j]>temp) {
-                    a[j+1]=a[j];
-                }else {
-                    break;
-                }
-            }
-            a[j+1]=temp;
-        }
+        directInsertSort(a);
 
         System.out.println();
         System.out.println("排序之后：");
         for (int i = 0; i < a.length; i++)
         {
             System.out.print(a[i] + " ");
+        }
+
+    }
+
+    public  static void directInsertSort(int [] array){
+
+        for (int i=1;i<array.length;i++){
+            int temp=array[i];
+            int j;
+            for ( j=i-1;j>=0;j--){
+                if (array[j]>temp){
+                    array[j+1]=array[j];
+                }else {
+                    break;
+                }
+            }
+            array[j+1]=temp;
         }
 
     }
